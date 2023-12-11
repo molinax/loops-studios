7b1cdf4f656b686093a3efef33f69f526e67d96c
molinax <arielmolina_55@hotmail.com>
2023-12-11 18:09:26 -0300

    icons component created

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -1,6 +1,10 @@
+import logo from '../../../public/images/logo.svg'
+
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className="container py-6 bg-slate-300">
+      <img src={logo} alt="Loopstudios logo" />
+    </header>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ export default {
         'dark-gray': 'hsl(0, 0%, 55%)',
         'very-dark-gray': 'hsl(0, 0%, 41%)'
       }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '3rem',
+        lg: '8rem',
+        xl: '9rem',
+        '2xl': '12rem'
+      }
     }
   },
   plugins: []
