236d62824a65c3613df0f293cee2e39e91b89192
molinax <arielmolina_55@hotmail.com>
2023-12-11 17:17:11 -0300

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        black: 'hsl(0, 0%, 0%)',
+        'dark-gray': 'hsl(0, 0%, 55%)',
+        'very-dark-gray': 'hsl(0, 0%, 41%)'
+      }
+    }
+  },
+  plugins: []
+}
