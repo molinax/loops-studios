d0af4b30377531f414b4208b6780c64c0a1367c9
molinax <arielmolina_55@hotmail.com>
2023-12-11 18:36:32 -0300

    nav-desktop created and used in header component

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -3,18 +3,22 @@ import { HamburguerIcon, CloseIcon } from '../Icons'
 
 import logo from '../../../public/images/logo.svg'
 import NavMobile from './NavMobile'
+import NavDesktop from './NavDesktop'
 
 const Header = () => {
   const [isMenuOpened, setIsMenuOpened] = useState(false)
   const switchMenu = () => setIsMenuOpened(!isMenuOpened)
 
   return (
-    <header className="container py-10 flex justify-between items-center bg-slate-200">
+    <header className="container py-10 flex justify-between items-center bg-slate-300">
       <img src={logo} alt="Loopstudios logo" />
 
-      { isMenuOpened
-        ? <><CloseIcon onClick={switchMenu} /> <NavMobile /></>
-        : <HamburguerIcon onClick={switchMenu} /> }
+      <div className="block md:hidden">
+        { isMenuOpened
+          ? <><CloseIcon onClick={switchMenu} /> <NavMobile /></>
+          : <HamburguerIcon onClick={switchMenu} /> }
+      </div>
+      <NavDesktop />
     </header>
   )
 }
---
+++ src/components/containers/NavDesktop.jsx
@@ -0,0 +1,25 @@
+const NavDesktop = () => {
+  return (
+    <nav className="hidden md:block">
+      <ul className="flex gap-6">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Careers</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavDesktop
