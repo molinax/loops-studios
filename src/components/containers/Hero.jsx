fd6ea75175a1c8e7da80b5fee70a8e0871afbdf3
molinax <arielmolina_55@hotmail.com>
2023-12-11 22:30:08 -0300

    hero image height modified

--- src/components/containers/Hero.jsx
+++ src/components/containers/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="container flex sm:py-12">
       <picture>
         <source media="(max-width: 640px)" srcSet={heroMobile} />
-        <img src={heroDesktop} alt="Immersive experience" className="absolute top-0 left-0 w-screen -z-20 sm:object-cover sm:h-[30rem]" />
+        <img src={heroDesktop} alt="Immersive experience" className="absolute top-0 left-0 w-screen -z-20 object-cover h-auto max-h-[40rem] sm:h-[30rem]" />
       </picture>
       <div className="relative -z-20 -bottom-20 p-6 border-2 border-white large-title-text text-white w-auto max-w-xl sm:bottom-0">
         <h1 className="large-title-text text-white">
